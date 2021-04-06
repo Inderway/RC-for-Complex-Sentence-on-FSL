5e12713060787889ad0090a02848730eef0d70e1
Inderway <1040214708@qq.com>
2021-04-06 14:13:49 +0800

    u

--- test_codes/test_code.py
+++ test_codes/test_code.py
@@ -3,6 +3,7 @@
 import torch
 from torch_geometric.data import Data
 
+device=torch.device('cuda')
 x=torch.tensor([[-1], [0], [1]], dtype=torch.float)
 # 定义两次边以表示无向
 edge_index=torch.tensor([[0, 1, 1, 2],
@@ -13,4 +14,12 @@ edge_index2=torch.tensor([[0, 1],
                            [1, 0],
                            [1, 2],
                            [2, 1]], dtype=torch.long)
-data=Data(x=x, edge_index2=edge_index2.t().contiguous())
+# data为dictionary
+data=Data(x=x, edge_index=edge_index2.t().contiguous())
+#data=data.to(device)
+
+print(data)
+
+from torch_geometric.datasets import TUDataset
+dataset=TUDataset(root='data/tmp/ENZYMES', name='ENZYMES')
+
